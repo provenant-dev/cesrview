be12c98efef59c1a953d5fcc36aec8e82ab000e7
s-a-tanjim <shoebtanjim@outlook.com>
2023-01-30 12:30:19 +0600

    Improve Html generation logic

--- plugins/cesrParser/generateHTML.js
+++ plugins/cesrParser/generateHTML.js
@@ -1,18 +1,12 @@
 var stack = [];
+var lineNumber = 0;
 
 function addTabSpace(multiplier=3) {  // multiplier = tab size
   return "&nbsp;".repeat(stack.length * multiplier);
 }
 
-var lineNumber = 0;
-function addNewLine() {
-  lineNumber++;
-  if(lineNumber === 1)
-    return "<span class='op-line-number'>" + lineNumber + "</span>";
-  return "<br><span class='op-line-number'>" + lineNumber + "</span>";
-}
 
-function addCSSClass(value,meta){
+function addCSSClass(value, meta){
   let meta_count_data = "";
   if(meta.hasOwnProperty('type')) {
     meta_count_data = " meta-cesr-count-code='" + meta.counter_code + "' meta-cesr-count-code-int='" + meta.counter_code_int + "' ";
@@ -20,24 +14,31 @@ function addCSSClass(value,meta){
   return "<span class='code_" + meta.code + "' meta-cesr-code='"+meta.code+"' "+ meta_count_data +" >" + value + "</span>";
 }
 
+
+function generateLine(data) {
+  lineNumber++;
+  return "<div class='op-line-container'><div class='op-line-number'>"+ lineNumber +"</div><div class='op-line-data'>"+ data +"</div></div>";
+}
+
 function __generateCESRHtml(object) {
   var generated = "";
   for (let i in object) {
     if(isNaN(Number(i))){
-      generated = generated + addNewLine() + addTabSpace() + i + ": "
+      let temp = addTabSpace() + i + ": "
 
       if(object[i] && object[i].hasOwnProperty('length') && object[i].length > 1) {
-        generated = generated + "[ ";
+        temp += "[ ";
       } else {
-        generated = generated + "{ ";
+        temp += "{ ";
       }
+      generated += generateLine(temp);
       stack.push(i);
     }
 
     if(object[i].hasOwnProperty('____meta')) {
-      generated = generated + addNewLine() + addTabSpace() + addCSSClass(object[i].____value, object[i].____meta) + ",";
+      generated += generateLine(addTabSpace() + addCSSClass(object[i].____value, object[i].____meta) + ",");
     } else if (object[i].hasOwnProperty('____value')){
-      generated = generated + addNewLine() + addTabSpace() + object[i].____value + ",";
+      generated += generateLine(addTabSpace() + object[i].____value + ",");
     } else {
       generated += __generateCESRHtml(object[i])
     }
@@ -45,11 +46,10 @@ function __generateCESRHtml(object) {
     if(isNaN(Number(i))){
       stack.pop();
       if(object[i] && object[i].hasOwnProperty('length') && object[i].length > 1) {
-        generated = generated + addNewLine() + addTabSpace() + "]";
+        generated += generateLine(addTabSpace() + "]");
       } else {
-        generated = generated + addNewLine() + addTabSpace() + "}";
+        generated += generateLine(addTabSpace() + "}");
       }
-      
     }
   }
   return generated
